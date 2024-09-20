04050a5574f1e6517c69676dae838f9a7db61557
kedualmelkar <kedaralmelkar1410@gmail.com>
2024-09-20 13:09:26 +0530

    Add files via upload

---
+++ cjc.py
@@ -0,0 +1,31 @@
+marks=int(input("enter marks"))
+if marks>100:
+    print("P")
+elif marks<0:
+    print("F")
+else:
+    print("zero")
+
+
+print()
+
+
+
+
+sub1=float(input("enter sub1 marks"))
+sub2=float(input("enter sub2 marks"))
+total_marks=sub1+sub2
+print("total marks=",total_marks)
+persentage_marks=total_marks
+print("persentage marks=",persentage_marks)
+
+
+
+print()
+
+
+print("count of vowals")
+
+
+
+    
